2cdda81de0c986879d20119aecb2f2b1ff32f3f2
Vladosjan <150368423+Vladosjan@users.noreply.github.com>
2023-12-31 13:42:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let div = document.createElement("img"); 
-div.src = "For work.jpg";
-div.style.width = "100%";  
-div.style.height= "100%";  
-let div1 = document.createElement("div"); 
-div1.append(div); 
-document.body.append(div); 
+
